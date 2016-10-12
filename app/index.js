f149b8abff0b2c859c7b706855e29a2cfbd20278
Artur Omazda <artur@omazda.pl>
2016-10-12 22:15:31 +0200

    impored router and store to app

--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
-import DefaultLayout from './layout/default';
+import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import store from './reducers/store';
+import { routes } from './common/routes';
 
 import './common/styles/global.scss';
 
-render(
-  <DefaultLayout />,
-  document.getElementById('app')
-);
+render((
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+), document.getElementById('app'));
