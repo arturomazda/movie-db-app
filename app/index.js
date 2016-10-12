4f2b83c580574c27f4e4c58ae246fe0876f354ff
Artur Omazda <artur@omazda.pl>
2016-10-12 21:11:46 +0200

    imported global styles to app

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import DefaultLayout from './layout/default';
 
+import './common/styles/global.scss';
+
 render(
   <DefaultLayout />,
   document.getElementById('app')
