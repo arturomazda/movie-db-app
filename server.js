f9f470e01567caa042160e9f0b30bfe24c89e929
Artur Omazda <artur@omazda.pl>
2016-10-12 20:31:31 +0200

    added default server

---
+++ server.js
@@ -0,0 +1,14 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  noInfo: true,
+  stats: { colors: true },
+  historyApiFallback: true
+}).listen(9000, 'localhost', function (err) {
+    if (err) { console.error(err); }
+    console.log('Listening at localhost:9000');
+  });
