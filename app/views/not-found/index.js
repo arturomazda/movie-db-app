c66ee2e1947448ba1d2f605c030c953ab885854a
Artur Omazda <artur@omazda.pl>
2016-10-12 22:16:31 +0200

    404 View: added

---
+++ app/views/not-found/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './not-found.scss';
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div className="not-found">
+        <div>404</div>
+        <div>Page not found</div>
+      </div>
+    );
+  }
+}
