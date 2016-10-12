dc8ccbc88b3f7bd2fcf9a7af62501de4c9b603c5
Artur Omazda <artur@omazda.pl>
2016-10-12 22:16:03 +0200

    Movie View: added

---
+++ app/views/movie/index.js
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from 'react';
+
+import MovieDetails from '../../components/movie-details';
+
+import './movie-view.scss';
+
+export default class MovieView extends Component {
+  static propTypes = {
+    params: PropTypes.object.isRequired
+  }
+
+  render() {
+    const movieId = this.props.params.movieId;
+
+    return (
+      <div className="movie-view">
+        <MovieDetails movieId={movieId}/>
+      </div>
+    );
+  }
+}
